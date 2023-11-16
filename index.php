03c1330ccc5497f1c0f536147a2f67a36f787211
Ney1981 <joanmartwhit500@gmail.com>
2023-11-16 10:40:27 -0500

    Arreglos

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
                         <!-- <a href="estudiante.php" class="header__item">Estudiante</a>
                         <a href="docente.php" class="header__item">Docente</a> -->
                         <a href="admisiones.php" class="header__item">Admisiones</a>
-                        <a href="admisiones.php" class="header__item">Gestion documental</a>
+
                     </ul>
                 </nav>
             </div>
@@ -380,17 +380,22 @@
         </svg>
     </a>
 
-    <button class="button__fab">
-        <svg class="button__svg" xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-whatsapp"
-            width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-            stroke-linecap="round" stroke-linejoin="round">
-            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-            <path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9"></path>
-            <path d="M9 10a.5 .5 0 0 0 1 0v-1a.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a.5 .5 0 0 0 0 -1h-1a.5 .5 0 0 0 0 1">
-            </path>
-        </svg>
-        <div class="button__popup">
-            <p>Chatea con nosotros💬</p>
+    <button class="button__fab" onclick="window.open('https://wa.me/573144895006/', '_blank')">
+        <div id="block-chatwhatsapp" class="block block--code">
+            <div class="conte wa-web">
+                <div class="centered-content">
+                    <img src="ruta_de_la_imagen_del_logo_de_whatsapp.png" alt="" class="whatsapp-logo">
+                    <svg class="button__svg icon icon-tabler icon-tabler-brand-whatsapp" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                        <path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9"></path>
+                        <path d="M9 10a.5 .5 0 0 0 1 0v-1a.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a.5 .5 0 0 0 0 -1h-1a.5 .5 0 0 0 0 1">
+                        </path>
+                    </svg>
+                </div>
+                <div class="button__popup">
+                    <p>Chatea con nosotros💬</p>
+                </div>
+            </div>
         </div>
     </button>
     <footer>
